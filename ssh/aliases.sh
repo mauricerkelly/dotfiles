321447b14c6597044e9e27960f003eadacb26183
Maurice Kelly <osv-github@chatswood.org.uk>
2012-06-29 10:49:26 +0100

    Added GoReport SSH alias

--- ssh/aliases.sh
+++ ssh/aliases.sh
@@ -1,4 +1,5 @@
 alias sshb='ssh -p 351 ssh.chatswood.org.uk'
+alias sshgr='ssh -l root www.goreport.net'
 alias sshh='ssh -l root dev.gcdtech.com'
 alias sshtunnel='ssh -C2qtNfg -o ServerAliveInterval=600 -D 3351 -p 351 mkelly@ssh.chatswood.org.uk'
 alias sshvm='ssh redhat61'
