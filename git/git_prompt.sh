3a08626fcdf7c1285bc3d356dfe23e252666c77a
Maurice Kelly <osv-github@chatswood.org.uk>
2012-10-03 17:32:24 +0100

    Added colourful git prompt

---
+++ git/git_prompt.sh
@@ -0,0 +1,27 @@
+function _git_prompt() {
+    local git_status="`git status -unormal 2>&1`"
+    if ! [[ "$git_status" =~ Not\ a\ git\ repo ]]; then
+        if [[ "$git_status" =~ nothing\ to\ commit ]]; then
+            local ansi=41
+        elif [[ "$git_status" =~ nothing\ added\ to\ commit\ but\ untracked\ files\ present ]]; then
+            local ansi=41
+        else
+            local ansi=31
+        fi
+        if [[ "$git_status" =~ On\ branch\ ([^[:space:]]+) ]]; then
+            branch=${BASH_REMATCH[1]}
+            test "$branch" != master || branch=' '
+        else
+            # Detached HEAD.  (branch=HEAD is a faster alternative.)
+            branch="(`git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
+                echo HEAD`)"
+        fi
+        echo -n '\[\e[0;37;'"$ansi"';1m\]'"$branch"'\[\e[0m\] '
+    fi
+}
+
+function _prompt_command() {
+    PS1="`_git_prompt`"'[\u@\h - \w]\n$ '
+}
+
+PROMPT_COMMAND=_prompt_command
