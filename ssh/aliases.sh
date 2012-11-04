86f688614824ef7de9c2b76b2d4d96f7bfcdb0cb
Maurice Kelly <osv-github@chatswood.org.uk>
2012-11-04 20:39:51 +0000

    Added alias to MNWM

--- ssh/aliases.sh
+++ ssh/aliases.sh
@@ -1,5 +1,6 @@
 alias sshb='ssh -p 351 ssh.chatswood.org.uk'
 alias sshgr='ssh -l root www.goreport.net'
 alias sshh='ssh -l root dev.gcdtech.com'
+alias sshmnw='ssh cw1.default.chatswood.uk0.bigv.io'
 alias sshtunnel='ssh -C2qtNfg -o ServerAliveInterval=600 -D 3351 -p 351 mkelly@ssh.chatswood.org.uk'
 alias sshvm='ssh redhat61'
