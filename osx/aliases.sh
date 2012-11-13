530786fa5d437841d59472e519f30cd03e1f4bd1
Maurice Kelly <osv-github@chatswood.org.uk>
2012-11-13 14:50:03 +0000

    Added subl alias for Sublime Text 2

--- osx/aliases.sh
+++ osx/aliases.sh
@@ -1,1 +1,2 @@
 alias genctags='/Applications/BBEdit.app/Contents/Helpers/ctags --excmd=number --tag-relative=no --fields=+a+m+n+S -f tags -R "$PWD"'
+alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
