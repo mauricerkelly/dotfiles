70d9560cb2bcea91ec2314699af936c0a3fe015e
Maurice Kelly <osv-github@chatswood.org.uk>
2013-07-30 10:00:27 +0100

    Switch from ST2 to ST3 for subl alias

--- osx/aliases.sh
+++ osx/aliases.sh
@@ -1,4 +1,4 @@
 if is_mac; then
     alias genctags='/Applications/BBEdit.app/Contents/Helpers/ctags --excmd=number --tag-relative=no --fields=+a+m+n+S -f tags -R "$PWD"'
-    alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+    alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 fi
