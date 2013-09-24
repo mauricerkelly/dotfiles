8b91120fe138b77d74c303a66018f33f077c9b72
Maurice Kelly <osv-github@chatswood.org.uk>
2013-09-24 20:51:00 +0100

    Aliases for defaults and working on settings

--- osx/aliases.sh
+++ osx/aliases.sh
@@ -1,4 +1,9 @@
-if is_mac; then
-    alias genctags='/Applications/BBEdit.app/Contents/Helpers/ctags --excmd=number --tag-relative=no --fields=+a+m+n+S -f tags -R "$PWD"'
-    alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+if ! is_mac; then
+    return
 fi
+
+alias dr='defaults read'
+alias dwr='defaults write'
+alias genctags='/Applications/BBEdit.app/Contents/Helpers/ctags --excmd=number --tag-relative=no --fields=+a+m+n+S -f tags -R "$PWD"'
+alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+
