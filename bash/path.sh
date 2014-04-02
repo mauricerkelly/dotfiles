24f6f5eba1da5eeaf4099e3064578bf3e23ebdd3
Maurice Kelly <osv-github@chatswood.org.uk>
2014-04-02 09:08:02 +0100

    Removed PATH item and added git cf alias

--- bash/path.sh
+++ bash/path.sh
@@ -2,6 +2,5 @@ add_to_path /usr/local/mysql/bin
 add_to_path $HOME/bin
 add_to_path $HOME/scripts
 add_to_path /usr/local/share/npm/bin
-add_to_path $HOME/.rvm/bin
 add_to_path /usr/local/heroku/bin
 path_prepend /usr/local/bin
