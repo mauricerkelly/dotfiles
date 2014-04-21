62407a4256d8044024c1d1fc2557f5215439f27a
Maurice Kelly <osv-github@chatswood.org.uk>
2014-04-21 21:42:15 +0100

    Support for brew cask installations

---
+++ bash/functions/brew.sh
@@ -0,0 +1,42 @@
+function brew_package_is_installed() {
+  installed_packages=`brew list -1`
+  for installed_package in $installed_packages; do
+    if [ $installed_package == $1 ]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+function brew_cask_package_is_installed() {
+  brew_package_is_installed brew-cask
+  return $?
+}
+
+function brew_cask_is_installed() {
+  installed_casks=`brew cask list`
+  for installed_cask in $installed_casks; do
+    if [ $installed_cask == $1 ]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+function install_cask() {
+  brew_cask_is_installed $1
+  if [ $? == 1 ]; then
+    brew cask install $1
+    return 0
+  fi
+
+  echo "Cask $1 already installed - skipping..."
+  return 1
+}
+
+export -f brew_package_is_installed
+export -f brew_cask_package_is_installed
+export -f brew_cask_is_installed
+export -f install_cask
