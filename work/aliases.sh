3bdbdbdbb6be5df3bf87eee74fddcc9f3b2d8578
Maurice Kelly <osv-github@chatswood.org.uk>
2014-09-01 09:54:41 +0100

    Added a couple of work related aliases

--- work/aliases.sh
+++ work/aliases.sh
@@ -1,7 +1,8 @@
 DEV_DIR=/Users/mkelly/Dropbox/Development/Git
 LOCAL_DEV_DIR=/Users/mkelly/Development/SK
 alias cdev='cd $DEV_DIR'
-alias cdir='cd $LOCAL_DEV_DIR/ipad-register'
-alias cdsdk='cd $LOCAL_DEV_DIR/SKBackofficeSDK'
+alias cdir='cd $LOCAL_DEV_DIR/ipad-register; rvm use @register'
+alias cdsdk='cd $LOCAL_DEV_DIR/SKBackofficeSDK; rvm use @sdk'
+alias cdstg='cd "/Users/mkelly/Dropbox/Documents/Writing/Swift Translation Guide"'
 alias cdtr='cd $DEV_DIR/Trailer'
 alias cdxcp='cd $DEV_DIR/XCPretty'
