1231d588544ef6d07967783340e0111059985be1
Maurice Kelly <osv-github@chatswood.org.uk>
2014-11-16 20:08:20 +0000

    Added aliases for homebrew

--- homebrew/aliases.sh
+++ homebrew/aliases.sh
@@ -1,4 +1,6 @@
 if is_mac; then
+    alias b='brew'
+    alias bc='brew cask'
     alias cdc='cd $(brew --repository)/Library/Taps/caskroom/homebrew-cask'
     alias cdcv='cd $(brew --repository)/Library/Taps/caskroom/homebrew-versions'
 fi
