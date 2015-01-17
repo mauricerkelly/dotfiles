ad18e8173a8e826cdbb1b00289cd1d7b4005201e
Maurice Kelly <osv-github@chatswood.org.uk>
2015-01-17 18:58:03 +0000

    General updates to Atom, LLDB, and aliases

--- work/aliases.sh
+++ work/aliases.sh
@@ -1,6 +1,7 @@
 DEV_DIR=/Users/mkelly/Dropbox/Development/Git
 LOCAL_DEV_DIR=/Users/mkelly/Development/SK
 alias cdev='cd $DEV_DIR'
+alias cdevsk='cd $LOCAL_DEV_DIR'
 alias cdir='cd $LOCAL_DEV_DIR/ipad-register; rvm use @register'
 alias cdsdk='cd $LOCAL_DEV_DIR/SKBackofficeSDK; rvm use @sdk'
 alias cdstg='cd "/Users/mkelly/Dropbox/Documents/Writing/Swift Translation Guide"'
