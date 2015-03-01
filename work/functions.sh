5662da435be53337aa26b4d0f87875f859a2d4e7
Maurice Kelly <osv-github@chatswood.org.uk>
2015-03-01 16:52:29 +0000

    Remove hyphens from function names

--- work/functions.sh
+++ work/functions.sh
@@ -12,18 +12,18 @@ function skdiff() {
 
 }
 
-function show-citests() {
+function showcitests() {
   git update-index --no-assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests1.xcscheme
   git update-index --no-assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests2.xcscheme
   git update-index --no-assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests3.xcscheme
 }
 
-function hide-citests() {
+function hidecitests() {
   git update-index --assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests1.xcscheme
   git update-index --assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests2.xcscheme
   git update-index --assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests3.xcscheme
 }
 
 export -f skdiff
-export -f show-citests
-export -f hide-citests
+export -f showcitests
+export -f hidecitests
