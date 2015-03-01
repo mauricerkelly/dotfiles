991a0d5809835fbdbdfee6e597e622b915adc922
Maurice Kelly <osv-github@chatswood.org.uk>
2015-03-01 16:51:03 +0000

    Add dotfiles/bin to path

--- bash/path.sh
+++ bash/path.sh
@@ -3,4 +3,5 @@ add_to_path $HOME/bin
 add_to_path $HOME/scripts
 add_to_path /usr/local/share/npm/bin
 add_to_path /usr/local/heroku/bin
+add_to_path $HOME/.dotfiles/bin
 path_prepend /usr/local/bin
