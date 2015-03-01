8d28d3736e70f9bbd013bf60f65ec15fe39c6f7e
Maurice Kelly <osv-github@chatswood.org.uk>
2015-03-01 16:53:46 +0000

    Update OS X aliases

--- osx/aliases.sh
+++ osx/aliases.sh
@@ -2,9 +2,8 @@ if ! is_mac; then
     return
 fi
 
-alias cdlas='cd $HOME/Library/Application\ Support'
+alias cdas='cd $HOME/Library/Application\ Support'
+alias conv_tesco='iconv -c -f utf-8 -t ascii report.csv | tail -n +2 > stripped.csv'
 alias dr='defaults read'
 alias dwr='defaults write'
-alias genctags='/Applications/BBEdit.app/Contents/Helpers/ctags --excmd=number --tag-relative=no --fields=+a+m+n+S -f tags -R "$PWD"'
 alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
-
