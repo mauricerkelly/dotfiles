5f151afbebbaad230e180f8ea9760e77cc3d29f9
Maurice Kelly <osv-github@chatswood.org.uk>
2015-03-01 16:51:55 +0000

    Add aliases for brew cask

--- homebrew/aliases.sh
+++ homebrew/aliases.sh
@@ -1,6 +1,8 @@
 if is_mac; then
     alias b='brew'
     alias bc='brew cask'
+    alias bci='brew cask install'
+    alias bcu='brew cask update --verbose'
     alias cdc='cd $(brew --repository)/Library/Taps/caskroom/homebrew-cask'
     alias cdcv='cd $(brew --repository)/Library/Taps/caskroom/homebrew-versions'
 fi
