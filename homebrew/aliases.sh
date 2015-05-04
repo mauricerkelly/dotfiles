0cda1815a5b9dfb0ad3f0c69b93b9d73bd593fa7
Maurice Kelly <osv-github@chatswood.org.uk>
2015-05-04 12:02:26 +0100

    Added brew and cask install aliases

--- homebrew/aliases.sh
+++ homebrew/aliases.sh
@@ -1,8 +1,10 @@
 if is_mac; then
-    alias b='brew'
-    alias bc='brew cask'
-    alias bci='brew cask install'
-    alias bcu='brew cask update --verbose'
-    alias cdc='cd $(brew --repository)/Library/Taps/caskroom/homebrew-cask'
-    alias cdcv='cd $(brew --repository)/Library/Taps/caskroom/homebrew-versions'
+  alias b='brew'
+  alias bc='brew cask'
+  alias bci='brew cask install'
+  alias bcu='brew cask update --verbose'
+  alias cdc='cd $(brew --repository)/Library/Taps/caskroom/homebrew-cask'
+  alias cdcv='cd $(brew --repository)/Library/Taps/caskroom/homebrew-versions'
+  alias install_brew=`ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`
+  alias install_brew_cask=`install_cask caskroom/cask`
 fi
