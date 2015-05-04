016ded8c83e43499321acb14d82640b308caae3d
Maurice Kelly <osv-github@chatswood.org.uk>
2015-05-04 12:44:35 +0100

    Fix install_brew alias errors

--- homebrew/aliases.sh
+++ homebrew/aliases.sh
@@ -5,6 +5,6 @@ if is_mac; then
   alias bcu='brew cask update --verbose'
   alias cdc='cd $(brew --repository)/Library/Taps/caskroom/homebrew-cask'
   alias cdcv='cd $(brew --repository)/Library/Taps/caskroom/homebrew-versions'
-  alias install_brew=`ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`
-  alias install_brew_cask=`install_cask caskroom/cask`
+  alias install_brew='ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+  alias install_brew_cask='install_cask caskroom/cask'
 fi
