f87549a866993cc26b697fdfb2d83b32c952fadb
Maurice Kelly <osv-github@chatswood.org.uk>
2019-06-12 12:58:22 +0100

    Added rename_logs function

--- work/functions.sh
+++ work/functions.sh
@@ -1,39 +1,3 @@
-function skdiff() {
-  diff_dir=$1
-  sk_dev_dir="$HOME/Development/SK"
-  reg_dir="$sk_dev_dir/ipad-register/Pods/SKBackofficeSDK/SKBackofficeSDK/$diff_dir"
-  sdk_dir="$sk_dev_dir/SKBackofficeSDK/SKBackofficeSDK/$diff_dir"
-
-  if [ -e "$reg_dir" ]; then
-    ksdiff "$reg_dir" "$sdk_dir"
-  else
-    echo "Error: one or both of the directories does not exist: $reg_dir; $sdk_dir"
-  fi
-
-}
-
-function showcitests() {
-  git update-index --no-assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests1.xcscheme
-  git update-index --no-assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests2.xcscheme
-  git update-index --no-assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests3.xcscheme
-}
-
-function hidecitests() {
-  git update-index --assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests1.xcscheme
-  git update-index --assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests2.xcscheme
-  git update-index --assume-unchanged Register.xcodeproj/xcshareddata/xcschemes/CITests3.xcscheme
-}
-
-function copyDatabasesIntoPlace() {
-  destination_dir=$1
-  source_dir=${PWD}
-
-  cp -v "${source_dir}"/MineShaft.* "${destination_dir}/Documents/"
-  cp -v "${source_dir}"/SKBackgroundJob-BackOffice.* "${destination_dir}/Documents/"
-  cp -v "${source_dir}/Register.sqlite" "${destination_dir}/Library/Application Support/ShopKeep Register/"
-  cp -v "${source_dir}/com.shopkeep.register.plist" "${destination_dir}/Library/Preferences/"
-}
-
 function reset_integrator() {
   if [[ ${OPTIC_LINK_HOME} == "" ]]; then
     echo "No OPTIC_LINK_HOME environment variable set up"
@@ -70,3 +34,13 @@ if [[ $SHELL == "/usr/local/bin/bash" ]]; then
   export -f hidecitests
   export -f copyDatabasesIntoPlace
 fi
+
+
+function rename_logs() {
+  for file in `ls -1 *.log.*`
+  do 
+    suffix=${file#*log.}
+    prefix=${file%.log*}
+    mv $file $prefix.$suffix.log
+  done
+}
