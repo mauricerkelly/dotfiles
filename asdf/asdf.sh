cafc106fa8af8f4d8f51d4a4c2928c72441175fe
Maurice Kelly <osv-github@chatswood.org.uk>
2021-03-07 18:17:31 +0000

    Only set up ASDF on macOS

--- asdf/asdf.sh
+++ asdf/asdf.sh
@@ -1,2 +1,4 @@
-echo "Setting up asdf"
-test -s $(brew --prefix asdf)/asdf.sh && source $(brew --prefix asdf)/asdf.sh
+if is_mac; then
+  echo "Setting up asdf"
+  test -s $(brew --prefix asdf)/asdf.sh && source $(brew --prefix asdf)/asdf.sh
+fi
